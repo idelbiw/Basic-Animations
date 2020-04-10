d544e90ca3b24601cb79136dac3e97b274aa3216
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-04-09 21:52:45 -0400

    configured label for display with an emoji

--- Basic Animations/ViewController.swift
+++ Basic Animations/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLabel()
     }
 
+    var label: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    func configureLabel() {
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.text = "👌"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 96)
+        label.layer.borderWidth = 2
+        label.layer.cornerRadius = 12
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
 
 }
 
