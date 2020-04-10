815e7444ccd2a1b5fa6de452b994a3839da2a295
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-04-09 22:13:38 -0400

    Added a rotate button in a stack view

--- Basic Animations/ViewController.swift
+++ Basic Animations/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLabel()
+        configureButtons()
     }
 
     var label: UILabel!
@@ -22,7 +23,7 @@ class ViewController: UIViewController {
         
     }
     
-    func configureLabel() {
+    private func configureLabel() {
         label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -34,6 +35,32 @@ class ViewController: UIViewController {
         label.layer.cornerRadius = 12
         label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func configureButtons() {
+        let rotateButton = UIButton(type: .system)
+        rotateButton.translatesAutoresizingMaskIntoConstraints = false
+        rotateButton.setTitle("Rotate", for: .normal)
+        rotateButton.addTarget(self, action: #selector(rotateButtonTapped), for: .touchUpInside)
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.addArrangedSubview(rotateButton)
+        
+        NSLayoutConstraint.activate([
+            
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+            
+        ])
+    }
+    
+    @objc private func rotateButtonTapped() {
+        
     }
 
 }
