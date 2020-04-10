2f6ab52695f800ca25d7f9c394cd99e138600e10
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-04-09 21:35:24 -0400

    Created project

---
+++ Basic Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic Animations
+//
+//  Created by Waseem Idelbi on 4/9/20.
+//  Copyright © 2020 WaseemID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
